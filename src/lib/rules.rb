16ded9a86a75305269aac74e360975f5a7487662
c07een <c07een@258cd1b8-b061-c1c4-b2d9-caeda941bcbf>
2010-03-30 23:18:40 +0000

    Fixed a nice way of listing contained classes Take a look at lib/rules.rb:Rules::rules for some nice Ruby-magic
    git-svn-id: svn://svn.cs.umu.se/2010/5dv087/marcusk_c07een_c07fdg/trunk@37 258cd1b8-b061-c1c4-b2d9-caeda941bcbf

--- src/lib/rules.rb
+++ src/lib/rules.rb
@@ -1,16 +1,22 @@
-require 'rules/dummy_rule'
-
 
 module Rules
-  @rules = [DummyRule.new]
   def self.apply_rules(dice)
-    @rules.collect do |rule|
-      points, dice = rule.apply(dice)
+    rules.collect do |rule|
+      # TODO: Object creation in Ruby isn't great, possible optimization here
+      points, dice = rule.new.apply(dice)
       if dice.empty?
         points
       else
         points + apply_rules(dice)
       end
     end.max
+  end
+  
+  # Returns a sorted Array with all Classes in this module (no submodules)
+  def self.rules
+    self.constants.sort.collect do |const_name|
+      c = self.const_get( const_name )
+      c if c.is_a? Class
+    end
   end
 end
--- src/test/test_rules.rb
+++ src/test/test_rules.rb
@@ -2,12 +2,24 @@ require 'test/unit'
 
 $LOAD_PATH << 'lib' if !($:.include?("lib"))
 require 'rules'
+$rule_files = Dir.glob('lib/rules/*_rule.rb').sort
+$rule_files.each {|f| require f }
 
 class TestRule < Test::Unit::TestCase
 
   def test_all_rules_applied_to_no_dice_should_return_zero
     points = Rules.apply_rules( [] )
     assert_equal(0,points)
+  end
+  
+  def test_should_find_all_rules
+    found_rules = Rules::rules
+    real_rules = $rule_files.collect do |f|
+      # turns "apa_bepa" into "ApaBepa", can be done in a better way
+      camelcased = File.basename(f,'.rb').split('_').collect{|s| s.capitalize}.join
+      Rules.const_get( camelcased )
+    end
+    assert_equal(real_rules, found_rules)
   end
 end
 
