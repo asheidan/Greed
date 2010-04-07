6adca1ea4cd12aa943f6455ced1305a91e216b81
marcusk <marcusk@258cd1b8-b061-c1c4-b2d9-caeda941bcbf>
2010-04-07 11:14:15 +0000

    Removed forgotten requires and added comments
    git-svn-id: svn://svn.cs.umu.se/2010/5dv087/marcusk_c07een_c07fdg/trunk@68 258cd1b8-b061-c1c4-b2d9-caeda941bcbf

--- src/lib/players/clever_player.rb
+++ src/lib/players/clever_player.rb
@@ -1,11 +1,11 @@
-require 'rules/ones_and_fives_rule'
-require 'rules/street_rule'
-require 'rules/three_of_a_kind_rule'
 require 'players/dummy_player'
 require 'players/gambler_player'
 
 module Players
   # CleverPlayer plays differently depending on the current position.
+  # When it is not in the leading position it will take high risks and
+  # try to win as fast as possible. When it is leading it will save 
+  # direcly and not take any risks.
   class CleverPlayer < DummyPlayer
     def roll(dice)
       if position < 1.0
