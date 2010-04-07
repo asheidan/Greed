2a3e36509ad70529d55960c7d54ca1ae5035c1ed
marcusk <marcusk@258cd1b8-b061-c1c4-b2d9-caeda941bcbf>
2010-04-07 12:59:13 +0000

    Started on a sketch of the user interface.
    git-svn-id: svn://svn.cs.umu.se/2010/5dv087/marcusk_c07een_c07fdg/trunk@69 258cd1b8-b061-c1c4-b2d9-caeda941bcbf

---
+++ src/lib/ui.rb
@@ -0,0 +1,279 @@
+require 'tk'
+
+# Creating the window that is displayed when playing the game.
+def create_game_window
+  top = TkToplevel.new(root) do
+    title 'Game'
+  end
+  
+  TkFrame.new(top) do
+    TkLabelFrame.new(self) do
+      TkLabel.new(self) do
+        text 'Score for this turn: #  Roll: #'
+        pack
+      end
+      TkFrame.new(self) do
+        6.times do
+          TkCheckButton.new(self) do
+            text '#' # Die value
+            pack :side => :left
+          end
+        end
+        pack :fill => :x
+      end
+      TkFrame.new(self) do
+        TkButton.new(self) do
+          text 'Roll'
+          pack :side => :left
+        end
+        TkButton.new(self) do
+          text 'Stay'
+          pack :side => :left
+        end
+        pack
+      end
+      text 'Turn Status'
+      pack :fill => :x
+    end
+  
+    TkFrame.new(self) do
+      TkLabelFrame.new(self) do
+        TkScrollbox.new(self) do
+          pack :fill => :x
+        end
+        TkLabel.new(self) do
+          text 'Currently playing: name'
+          pack
+        end
+        TkLabel.new(self) do
+          text 'Win limit: #  Bust limit: #'
+          pack
+        end
+        text 'Game Status'
+        pack :fill => :x
+      end
+      pack :fill => :x
+    end
+    
+    TkFrame.new(self) do
+      TkButton.new(self) do
+        text 'Leave Game'
+        command do
+          top.destroy
+        end
+        pack
+      end
+      pack
+    end
+    
+    pady 2
+    padx 2
+    pack
+  end
+end
+
+# Creates a window from where a completely new game can be launched.
+def create_new_game_window
+  top = TkToplevel.new(root) do
+    title 'New Game'
+  end
+  
+  TkLabelFrame.new(top) do
+    TkLabel.new(self) do
+      text 'Win limit:'
+      grid :row => 0, :column => 0, :sticky => :e
+    end
+    TkEntry.new(self) do
+      grid :row => 0, :column => 1
+    end
+    TkLabel.new(self) do
+      text 'Bust limit:'
+      grid :row => 1, :column => 0, :sticky => :e
+    end
+    TkEntry.new(self) do
+      grid :row => 1, :column => 1
+    end
+    TkLabel.new(self) do
+      text 'Service:'
+      grid :row => 2, :column => 0, :sticky => :e
+    end
+    TkEntry.new(self) do
+      grid :row => 2, :column => 1
+    end
+    
+    text 'Game Details'
+    pady 2
+    padx 2
+    pack
+  end
+  
+  TkFrame.new(top) do
+    TkButton.new(self) do
+      text 'Cancel'
+      command do
+        top.destroy
+      end
+      grid :row => 0, :column => 0
+    end
+    
+    TkButton.new(self) do
+      text 'Launch'
+      command do
+        create_game_window
+        top.destroy
+      end
+      grid :row => 0, :column => 1
+    end
+    
+    pady 2
+    padx 2
+    pack
+  end
+end
+
+# Creates a window from where a connection can be made to an existing game.
+def create_existing_game_window
+  top = TkToplevel.new(root) do
+    title 'Existing Game'
+  end
+  
+  # Has to declare them here or configure won't find them
+  host_entry = service_entry = nil
+  
+  TkLabelFrame.new(top) do
+    TkFrame.new(self) do
+      choice = TkVariable.new
+      TkRadioButton.new(self) do
+        text 'Localhost'
+        value :localhost
+        variable choice
+        select
+        command do
+          host_entry.configure :state, :disabled
+          service_entry.configure :state, :disabled
+        end
+        pack :side => :left
+      end
+      TkRadioButton.new(self) do
+        text 'Remote'
+        value :remote
+        variable choice
+        deselect
+        command do
+          host_entry.configure :state, :normal
+          service_entry.configure :state, :normal
+        end
+        pack :side => :left
+      end
+      pack
+    end
+    
+    TkFrame.new(self) do
+      TkLabel.new(self) do
+        text 'Host:'
+        grid :row => 0, :column => 0, :sticky => :e
+      end
+      host_entry = TkEntry.new(self) do
+        state :disabled
+        grid :row => 0, :column => 1
+      end
+      TkLabel.new(self) do
+        text 'Service:'
+        grid :row => 1, :column => 0, :sticky => :e
+      end
+      service_entry = TkEntry.new(self) do
+        state :disabled
+        grid :row => 1, :column => 1
+      end
+      pack
+    end
+    
+    text 'Game Details'
+    pady 2
+    padx 2
+    pack
+  end
+  
+  TkFrame.new(top) do
+    TkButton.new(self) do
+      text 'Cancel'
+      command do
+        top.destroy
+      end
+      grid :row => 0, :column => 0
+    end
+    
+    TkButton.new(self) do
+      text 'Connect'
+      command do
+        create_game_window
+        top.destroy
+      end
+      grid :row => 0, :column => 1
+    end
+    
+    pady 2
+    padx 2
+    pack
+  end
+end
+
+# Creates the main (first?) window. From there new games can be launched and
+# connections can be made to existing ones.
+def create_main_window
+  root = TkRoot.new do
+    title 'Greed'
+  end
+  
+  TkLabel.new(root) do
+    text 'Shall we play a game?'
+    pack
+  end
+  
+  TkFrame.new(root) do
+    TkLabel.new(self) do
+      text 'Name:'
+      pack :side => :left
+    end
+    TkEntry.new(self) do
+      pack :side => :left
+    end
+    
+    pady 2
+    padx 2
+    pack
+  end
+    
+  TkFrame.new(root) do
+    TkButton.new(self) do
+      text 'New Game'
+      command do
+        create_new_game_window
+      end
+      grid :row => 1, :column => 0
+    end
+
+    TkButton.new(self) do
+      text 'Existing Game'
+      command do
+        create_existing_game_window
+      end
+      grid :row => 1, :column => 1
+    end
+
+    TkButton.new(self) do
+      text 'Quit'
+      command do
+         Tk.root.destroy
+       end
+      grid :row => 1, :column => 2
+    end
+    
+    pady 2
+    padx 2
+    pack
+  end
+end
+
+create_main_window
+Tk.mainloop
